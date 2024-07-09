49363d895c9d80425a414cc22561740d668560c4
ArthurVillela <arthur.villela1@gmail.com>
2024-07-09 14:26:40 +0200

    connecting to frontend

--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'genres',
     'directors',
     'reviews',
+    'corsheaders',
     'jwt_auth'
 ]
 
@@ -45,7 +46,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'project.urls'
 
